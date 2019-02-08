448fb0e0aad55260b3be1e5a4a7917c7a88ab5d1
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2019-02-08 23:49:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,34 +28,10 @@ client.user.setGame(`HooliGansᵀᵉᵃᵐ`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-
-
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`FALLEGA SYSTEM`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+var x = client.channels.get("543267866993229825");
+if (x) x.join();
 });
-
 
 client.on(`ready`, ()=>{
   client.setInterval(async function(){
